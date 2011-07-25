da3c2463a27a2ea43cd64246cf0a7e82310c603a
Javier de la Torre <jatorre@vizzuality.com>
2011-07-24 20:14:20 -0400

    started a rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenAidRegister::Application.config.secret_token = 'b599b8e4577f6bab747cca53ea4fea564b6445a60fc48cef56c3f7fc1655fb1e9f869ce8c85b7c574165e1ddffe1e5493b654495bf74a6a2ad2ed28de13b3f6a'
