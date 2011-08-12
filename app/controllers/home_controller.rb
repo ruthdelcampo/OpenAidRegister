b7060e64f9d71ff1a29a077bf137289ff348e366
Javier de la Torre <jatorre@vizzuality.com>
2011-08-12 18:42:40 -0400

    added first template

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  
+  def show
+    
+  end
+  
+  
+end
---
+++ test/unit/helpers/home_controller_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class HomeControllerHelperTest < ActionView::TestCase
+end
