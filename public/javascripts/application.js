1951fa85413ea1ae9e6bc1e6137368bb3506410c
Javier de la Torre <jatorre@gmail.com>
2011-09-24 15:46:04 -0400

    example use of gmaps

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,17 +1,35 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+var map;
+var markers = [];
 
-<script type="text/javascript">
-  $(document).ready(function(){ 
+$(document).ready(function(){ 
     var latlng = new google.maps.LatLng(-34.397, 150.644);
     var myOptions = {
       zoom: 8,
       center: latlng,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById("map_canvas"),
+    map = new google.maps.Map(document.getElementById("map_canvas"),
         myOptions);
-  });
 
-</script>
+   //Add the listener to add a marker
+	google.maps.event.addListener(map, 'click', addMarker);
+
+
+
+});
+
+function addMarker(event) {
+	marker = new google.maps.Marker({
+	    map:map,
+	    draggable:true,
+	    position: event.latLng
+	  });
+	markers.push(marker);
+}
+
+function generateWkt() {
+
+}
