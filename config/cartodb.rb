49a449c19d41bbb44c427a8c02e93664aa07e6ec
Fernando Espinosa <fer@ferdev.com>
2011-10-03 16:41:57 +0200

    Moves cartodb.rb initializer to initializers folder

--- config/cartodb.rb
+++
@@ -1,3 +0,0 @@
-cartodb_settings = YAML.load_file(Rails.root.join('config/cartodb_config.yml'))[Rails.env.to_s]
-cartodb_settings = cartodb_settings.merge(Hash[ENV].slice(hash.keys))
-CartoDB::Init.start cartodb_settings
