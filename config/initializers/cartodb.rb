82b9c956d170ee2a310304c09f965706a1369f99
Fernando Espinosa <fer@ferdev.com>
2011-10-03 16:58:29 +0200

    Requires cartodb-rb-client lib in cartodb initializer

--- config/initializers/cartodb.rb
+++ config/initializers/cartodb.rb
@@ -1,3 +1,5 @@
+require 'cartodb-rb-client'
+
 cartodb_settings = YAML.load_file(Rails.root.join('config/cartodb_config.yml'))[Rails.env.to_s] || {} rescue {}
 cartodb_settings = cartodb_settings.merge(Hash[ENV].slice(cartodb_settings.keys))
 CartoDB::Init.start cartodb_settings
