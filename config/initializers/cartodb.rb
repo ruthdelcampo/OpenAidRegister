3e832b53fb00ce5bb0faab1e75fe2c5db7be5166
Fernando Espinosa <fer@ferdev.com>
2011-10-03 17:01:34 +0200

    Doesn't slice cartodb_settings default keys

--- config/initializers/cartodb.rb
+++ config/initializers/cartodb.rb
@@ -1,5 +1,3 @@
-require 'cartodb-rb-client'
-
 cartodb_settings = YAML.load_file(Rails.root.join('config/cartodb_config.yml'))[Rails.env.to_s] || {} rescue {}
-cartodb_settings = cartodb_settings.merge(Hash[ENV].slice(cartodb_settings.keys))
+cartodb_settings = cartodb_settings.merge(Hash[ENV])
 CartoDB::Init.start cartodb_settings
