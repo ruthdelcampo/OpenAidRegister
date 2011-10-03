c8a2b239442dff6d78c9ab1b552702f7a5964aa5
Fernando Espinosa <fer@ferdev.com>
2011-10-03 16:53:32 +0200

    Removes invalid cartodb initializer

--- config/initializers/cartodb.rb
+++ config/initializers/cartodb.rb
@@ -1,3 +1,3 @@
 cartodb_settings = YAML.load_file(Rails.root.join('config/cartodb_config.yml'))[Rails.env.to_s] || {} rescue {}
-cartodb_settings = cartodb_settings.merge(Hash[ENV].slice(hash.keys))
+cartodb_settings = cartodb_settings.merge(Hash[ENV].slice(cartodb_settings.keys))
 CartoDB::Init.start cartodb_settings
