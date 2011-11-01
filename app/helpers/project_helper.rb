4d1c0df67de93cb1afed7a43240655ae42ed6350
Fernando Espinosa <fer@ferdev.com>
2011-11-01 20:40:55 +0100

    Fixes a bug when showing the project form empty

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -1,5 +1,5 @@
 module ProjectHelper
   def current_geo_level?(detail)
-    @reverse_geo.first.level_detail == detail
+    @reverse_geo.first.level_detail == detail if @reverse_geo.present?
   end
 end
