aade822f5caef2c5f280f94146c734b72e5dc644
Fernando Espinosa <fer@ferdev.com>
2011-11-01 20:10:01 +0100

    Fixes dates not being shown in existing projects

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def format_date(date)
-    date.strftime('%m/%d/%Y') and return if date.present? && date.is_a?(Date)
+    return date.strftime('%m/%d/%Y') if date.present? && date.is_a?(Date)
     date
   end
 
