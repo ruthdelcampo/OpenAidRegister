46239330f30c99d108fbc8364b6cf8dfd989a977
Claudio Perez Gamayo <crossblaim@gmail.com>
2012-01-19 19:29:04 +0100

    added the file again with the proper capitalization

---
+++ app/models/project.rb
@@ -0,0 +1,130 @@
+class Project
+
+  PRIMARY_KEY = "cartodb_id"
+  ATTRIBUTES = [
+                "aid_type",
+                "budget",
+                "budget_currency",
+                "collaboration_type",
+                "contact_email",
+                "contact_name",
+                "description",
+                "end_date",
+                "finance_type",
+                "flow_type",
+                "language",
+                "org_role",
+                "organization_id",
+                "program_guid",
+                "project_guid",
+                "result_description",
+                "result_title",
+                "start_date",
+                "tied_status",
+                "title",
+                "website",
+               ]
+
+  ATTRS_WITH_KEY = ATTRIBUTES.push PRIMARY_KEY
+
+  # find Project by id
+  #----------------------------------------------------------------------
+
+  def self.find(id, organization_id=nil)
+    if organization_id
+      sql = "SELECT #{ATTRS_WITH_KEY.join(',')} FROM projects WHERE cartodb_id = ? AND organization_id = ?"
+      result = Oar::execute_query(sql, id, organization_id)
+    else
+      sql = "select #{ATTRS_WITH_KEY.join(',')} FROM projects WHERE cartodb_id = ?"
+      result = Oar::execute_query(sql, id)
+    end
+    result.rows.first
+  end
+
+  # find the last project by organization_id
+  #----------------------------------------------------------------------
+  def self.last_by_organization_id(organization_id)
+    sql = "SELECT #{ATTRS_WITH_KEY.join(',')} from PROJECTS WHERE organization_id=? ORDER BY cartodb_id DESC LIMIT 1"
+    result = Oar::execute_query(sql, organization_id)
+    result.rows.first
+  end
+
+  # CREATE A PROGRAM
+  #----------------------------------------------------------------------
+
+  def self.create(params, organization_id, start_date, end_date)
+    # create the project
+    sql="INSERT INTO PROJECTS (organization_id, title, description, org_role, language,
+         project_guid, start_date, end_date, budget, budget_currency, website,
+         program_guid, result_title, result_description, collaboration_type,
+         tied_status, aid_type, flow_type, finance_type, contact_name, contact_email) VALUES
+                 (?, '?', '?','?',
+                 '?',
+                 '?', ?, ?, '?',
+                 '?', '?', '?', '?',
+                 '?',
+                 '?','?',
+                 '?',
+                 '?','?',
+                 '?',
+                 '?')"
+    Oar::execute_query(sql, organization_id,
+                       params[:title],
+                       params[:description],
+                       params[:org_role],
+                       params[:language],
+                       params[:project_guid],
+                       start_date,
+                       end_date,
+                       params[:budget],
+                       params[:budget_currency],
+                       params[:website],
+                       params[:program_guid],
+                       params[:result_title],
+                       params[:result_description],
+                       params[:collaboration_type],
+                       params[:tied_status],
+                       params[:aid_type],
+                       params[:flow_type],
+                       params[:finance_type],
+                       params[:contact_name],
+                       params[:contact_email])
+
+    # return the new project
+    Project.last_by_organization_id(organization_id)
+  end
+
+  def self.update(project_id, params, start_date, end_date)
+#it is an existing project do whatever
+    sql="UPDATE projects SET title='?', description ='?', org_role ='?',
+    language= '?', project_guid='?', start_date=?, end_date=?, budget='?', budget_currency='?',
+     website='?', program_guid = '?', result_title='?',
+     result_description='?', collaboration_type='?',tied_status ='?',
+     aid_type ='?', flow_type ='?',
+     finance_type ='?',contact_name='?', contact_email='?' WHERE cartodb_id='?'"
+
+    Oar::execute_query(sql,
+                  params[:title],
+                  params[:description],
+                  params[:org_role],
+                  params[:language],
+                  params[:project_guid],
+                  start_date,
+                  end_date,
+                  params[:budget],
+                  params[:budget_currency],
+                  params[:website],
+                  params[:program_guid],
+                  params[:result_title],
+                  params[:result_description],
+                  params[:collaboration_type],
+                  params[:tied_status],
+                  params[:aid_type],
+                  params[:flow_type],
+                  params[:finance_type],
+                  params[:contact_name],
+                  params[:contact_email],
+                  project_id)
+
+  end
+end
