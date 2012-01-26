681ec0278a0f553075c8cd90c9678fd62d287c2b
Claudio Perez Gamayo <crossblaim@gmail.com>
2012-01-26 16:23:32 +0100

    fixed markers when edit a project, now if the form has errors, the edited markers are displayed in the map as expected

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -73,12 +73,14 @@ class ProjectsController < ApplicationController
     @project_data[:sectors] = ProjectSector.kv_ids_where_project_id(params[:id])
     # Select partner organizations and form the array
     @participating_orgs = ProjectPartnerorganization.by_project_id(params[:id])
+
     # geo data
     @project_data[:reverse_geo] = ReverseGeo.by_project_id(params[:id])
     # @reverse_geo will be used for painting the points in the map with jquery
     (@project_data[:reverse_geo] || []).each_with_index do |row, index|
       @project_data[:latlng][index]= row[:latlng]
     end
+
     # transactions
     # Select partner organizations and form the array
     @project_data[:transaction_list] = ProjectTransaction.by_project_id(params[:id])
@@ -91,11 +93,17 @@ class ProjectsController < ApplicationController
   #----------------------------------------------------------------------
   def update
     @errors = []
-  #  @latlng = []
     @start_date = "null"
     @end_date   = "null"
     @project_data = params
+
+    # @reverse_geo will be used for painting the points in the map with jquery
+    @project_data[:latlng] = []
+    (@project_data[:reverse_geo] || []).each_with_index do |row, index|
+      @project_data[:latlng][index]= row[:latlng]
+    end
     @latlng = @project_data[:latlng]
+
     validate_params
     # there has been errors print them on the template AND EXIT
     if @errors.count > 0
